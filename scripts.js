2dc6bc9f4702eefdc0ccd98d6a67e76479112b21
Leader4syria <ahmadhass20a@gmail.com>
2025-02-08 21:35:44 +0300

    scripts.js

--- scripts.js
+++ scripts.js
@@ -8,12 +8,14 @@ async function convertCurrency() {
         return;
     }
 
-    const apiKey = 'b1cbda66dda4e999a7ffa990'; // استبدل بمفتاح API الخاص بك
+    const apiKey = 'YOUR_API_KEY'; // استبدل بمفتاح API الخاص بك
     const url = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${fromCurrency}/${toCurrency}/${amount}`;
 
     try {
         const response = await fetch(url);
         const data = await response.json();
+
+        console.log(data); // طباعة الـ response للتحقق من الأخطاء
 
         if (data.result === 'success') {
             const convertedAmount = data.conversion_result;
@@ -21,10 +23,10 @@ async function convertCurrency() {
             document.getElementById('result').innerText = `المبلغ المحول: ${convertedAmount.toFixed(2)} ${toCurrency}`;
             document.getElementById('exchangeRateInfo').innerText = `سعر الصرف الحالي: 1 ${fromCurrency} = ${exchangeRate.toFixed(4)} ${toCurrency}`;
         } else {
-            alert("حدث خطأ أثناء الحصول على سعر الصرف");
+            alert("حدث خطأ أثناء الحصول على سعر الصرف: " + data.error_type);
         }
     } catch (error) {
         console.error('Error:', error);
         alert("حدث خطأ أثناء الاتصال بالخادم");
     }
-}''
+}
